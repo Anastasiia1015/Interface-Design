ae5e82515a2e5b66794ce962cdc4882d619e1a83
Anastasiia Peliukhovska <119167044+Anastasiia1015@users.noreply.github.com>
2024-05-26 13:22:54 +0200

    added payment form

--- scripts/payment.js
+++ scripts/payment.js
@@ -21,3 +21,20 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+document.addEventListener('DOMContentLoaded', function () {
+    const cardNumberInput = document.getElementById('card_number');
+    const cvvInput = document.getElementById('cvv');
+
+    cardNumberInput.addEventListener('input', function (e) {
+        let value = e.target.value.replace(/\D/g, '');
+        if (value.length > 16) {
+            value = value.substring(0, 16);
+        }
+        const formattedValue = value.replace(/(\d{4})(?=\d)/g, '$1-');
+        e.target.value = formattedValue;
+    });
+
+    cvvInput.addEventListener('input', function (e) {
+        e.target.value = e.target.value.replace(/\D/g, '').substring(0, 3);
+    });
+});
