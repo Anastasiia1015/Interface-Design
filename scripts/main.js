cc15fc4a6408265f32eadf431213e0134b24008d
Yustyna Liubychenko <ustinalubicenko7@gmail.com>
2024-06-02 15:45:05 +0300

    project

--- scripts/main.js
+++ scripts/main.js
@@ -300,3 +300,71 @@ document.getElementById('submit-account').addEventListener('click', function(eve
     var accountBox = document.getElementById('account-box');
     accountBox.style.display = 'none';
 });
+
+document.getElementById('login-button').addEventListener('click', function() {
+    var loginBox = document.getElementById('login-box');
+    if (loginBox.style.display === 'none' || loginBox.style.display === '') {
+        loginBox.style.display = 'block';
+    } else {
+        loginBox.style.display = 'none';
+    }
+});
+
+document.getElementById('submit-login').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent default anchor behavior
+    var loginBox = document.getElementById('login-box');
+    loginBox.style.display = 'none';
+});
+document.getElementById('account-button').addEventListener('click', function() {
+    var accountBox = document.getElementById('account-box');
+    if (accountBox.style.display === 'none' || accountBox.style.display === '') {
+        accountBox.style.display = 'block';
+    } else {
+        accountBox.style.display = 'none';
+    }
+});
+
+document.getElementById('submit-account').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent default anchor behavior
+    var accountBox = document.getElementById('account-box');
+    accountBox.style.display = 'none';
+});
+// For the thumbnail demo! :]
+
+var count = 1
+setTimeout(demo, 500)
+setTimeout(demo, 700)
+setTimeout(demo, 900)
+setTimeout(reset, 2000)
+
+setTimeout(demo, 2500)
+setTimeout(demo, 2750)
+setTimeout(demo, 3050)
+
+
+var mousein = false
+function demo() {
+    if(mousein) return
+    document.getElementById('demo' + count++)
+        .classList.toggle('hover')
+
+}
+
+function demo2() {
+    if(mousein) return
+    document.getElementById('demo2')
+        .classList.toggle('hover')
+}
+
+function reset() {
+    count = 1
+    var hovers = document.querySelectorAll('.hover')
+    for(var i = 0; i < hovers.length; i++ ) {
+        hovers[i].classList.remove('hover')
+    }
+}
+
+document.addEventListener('mouseover', function() {
+    mousein = true
+    reset()
+})
